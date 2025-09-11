80ce8eee60043bdb969f2776c8dbc58bfb30e0cb
eunice-jpg <signeeunice7@gmail.com>
2025-09-11 22:20:52 +0100

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,18 @@
-import Signup from "./pages/SignUp";
+import React from 'react';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import Signup from './pages/SignUp';
+import './App.css'; 
 
 function App() {
-  return <Signup />;
+  return (
+    <Router>
+      <div className="App">
+        <Routes>
+          <Route path="/signup" element={<Signup />} />
+        </Routes>
+      </div>
+    </Router>
+  );
 }
 
 export default App;
