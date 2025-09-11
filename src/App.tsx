f133c4c76e4ef606090bc6e6c6e4e831525fc1d5
eunice-jpg <signeeunice7@gmail.com>
2025-09-11 23:00:57 +0100

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-import Signup from './pages/SignUp';
+import SignUp from './pages/SignUp';
 import './App.css'; 
 
 function App() {
